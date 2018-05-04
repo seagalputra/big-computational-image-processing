4c53dfa4f4b6932bed1fc451f9bac9ac08ff086f
seagalputra <seagalputra@gmail.com>
2018-05-04 22:25:20 +0700

    Add binary conversion in image

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import datetime
 import os
 
 def title_bar(host_id):
+    
     os.system('clear')
 
     print("--------------------------------------")
@@ -13,6 +14,7 @@ def title_bar(host_id):
     print("Hello ",host_id)
 
 def load_images():
+    # Implement load image in client-side
     print("Please load image first before going to next menu..")
     filename = input('Input your image filename (include file extensions) : ')
     # For uploading image to server
@@ -22,11 +24,13 @@ def load_images():
         result = proxy.image_upload(data, date, host_id)
 
 def user_menu():
+    # Client menu program
     print("1. Rotate Image")
     print("2. Blur Image")
     print("3. Convert image to grayscale")
-    print("4. Download the image file")
-    print("5. Exit")
+    print("4. Convert image to binary")
+    print("5. Download the image file")
+    print("6. Exit")
 
     return input("Choose menu : ")
 
@@ -34,6 +38,7 @@ def user_menu():
 # Change the ip address port location in the server
 proxy = xmlrpc.client.ServerProxy("http://localhost:8000")
 
+# Get computer client hostname and date
 host_id = str(socket.gethostname())
 date = str(datetime.datetime.now().date())
 
@@ -42,7 +47,7 @@ choice = 0
 title_bar(host_id)
 load_images()
 
-while(choice != '5'):
+while(choice != '6'):
     choice = user_menu()
     # Respond user choice
     if (choice == '1'):
@@ -53,6 +58,7 @@ while(choice != '5'):
             proxy.imrotate(date, host_id, degree)
             print("Image rotation success!")
         except:
+            # Catch if image failed to rotate
             print("Image rotation failed!")
     elif (choice == '2'):
         title_bar(host_id)
@@ -75,6 +81,15 @@ while(choice != '5'):
             print("Color conversion is failed!")
     elif (choice == '4'):
         title_bar(host_id)
+        # Convert image from rgb to binary image in server
+        try:
+            proxy.imthresh(date, host_id)
+            print("Image successfully convert in binary!")
+        except:
+            # Catch if convert image to binary is failed
+            print("Convert image to binary failed!")
+    elif (choice == '5'):
+        title_bar(host_id)
         # Download image from server
         new_filename = input("Please write the new image filename (include file extensions) : ")
         try:
@@ -85,6 +100,6 @@ while(choice != '5'):
         except:
             # Catch if download function is failed
             print("Image download failed!")
-    elif (choice == '5'):
+    elif (choice == '6'):
         title_bar(host_id)
         print("Thank you for using this application")
--- server.py
+++ server.py
@@ -7,6 +7,17 @@ from skimage import color
 from skimage import filters
 
 def image_upload(filedata, date, host_id):
+    """Function for uploading image from client program to server
+    
+    Arguments:
+        filedata {Image data} -- Read data from specify data file
+        date {String} -- Date from computer's client
+        host_id {String} -- Host id from computer's client
+    
+    Returns:
+        N-Dimensional Matrix -- Return image file in server-side
+    """
+
     with open(date+host_id+'.jpg', 'wb') as handle:
         data = filedata.data
         handle.write(data)
@@ -14,27 +25,75 @@ def image_upload(filedata, date, host_id):
         return True
 
 def image_download(date, host_id):
+    """Function to download image from server to client
+    
+    Arguments:
+        date {String} -- Date from computer's client
+        host_id {String} -- Host id from computer's client
+    
+    Returns:
+        N-Dimensional Matrix -- Return image in client-side
+    """
+
     handle = open(date+host_id+'.jpg', 'rb')
     return xmlrpc.client.Binary(handle.read())
     handle.close()
 
 def imrotate(date, host_id, angle):
+    """Function to rotate image using pecify angle in server
+    
+    Arguments:
+        date {String} -- Date from computer's client 
+        host_id {String} -- Host id from computer's client
+        angle {integer} -- Specify angle to rotate image
+    """
+
     image = io.imread(date+host_id+'.jpg')
     rotate = transform.rotate(image, angle)
     print('Image Rotated with', angle,' degree')
     io.imsave(date+host_id+'.jpg', rotate)
 
 def imblur(date, host_id, s):
+    """Function to blur the image
+    
+    Arguments:
+        date {String} -- Date from computer's client
+        host_id {String} -- Host id from computer's client
+        s {integer} -- Sigma parameter for kernel blur
+    """
+
     image = io.imread(date+host_id+'.jpg')
     blur = filters.gaussian(image, sigma=s)
     print('Image blur success')
     io.imsave(date+host_id+'.jpg', blur)
 
 def im2gray(date, host_id):
+    """Function for convert image to grayscale
+    
+    Arguments:
+        date {String} -- Date from computer's client
+        host_id {String} -- Host id from computer's client
+    """
+
     image = io.imread(date+host_id+'.jpg')
     gray = color.rgb2gray(image)
     print('Image successfuly convert to grayscale')
     io.imsave(date+host_id+'.jpg', gray)
+
+def imthresh(date, host_id):
+    """Function to convert image into binary image
+    
+    Arguments:
+        date {String} -- Date from computer's client
+        host_id {String} -- Host id from computer's client
+    """
+
+    image = io.imread(date+host_id+'.jpg')
+    gray = color.rgb2gray(image)
+    thresh = filters.threshold_mean(gray)
+    binary = gray > thresh
+    print("Image successfully threshold!")
+    io.imsave(date+host_id+'.jpg', binary)
 
 # Main Server Program
 # Change the ip address and port if necessary    
@@ -46,4 +105,5 @@ server.register_function(image_download, 'image_download')
 server.register_function(imrotate, 'imrotate')
 server.register_function(imblur, 'imblur')
 server.register_function(im2gray, 'im2gray')
+server.register_function(imthresh, 'imthresh')
 server.serve_forever()
